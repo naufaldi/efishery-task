beeb373b01f592be224a2f4cf29952933d9319dd
naufaldi <naufaldi.rafif@gmail.com>
2022-01-16 20:31:29 +0700

    fix: directory file, ts config

---
+++ src/components/Layout/Header.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react'
+import Link from 'next/link'
+
+const Header:FC = () => {
+ return (
+  <header className="navbar">
+      <nav>
+        <Link href="/">
+          <a>Home</a>
+        </Link>{' '}
+        |{' '}
+        <Link href="/about">
+          <a>About</a>
+        </Link>{' '}
+        |{' '}
+        <Link href="/users">
+          <a>Users List</a>
+        </Link>{' '}
+        | <a href="/api/users">Users API</a>
+      </nav>
+    </header>
+ )
+}
+
+export default Header
