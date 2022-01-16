a028dbbcca0415af37fbd6c1782e26f401614206
naufaldi <naufaldi.rafif@gmail.com>
2022-01-16 20:31:55 +0700

    fix:directory,ts config

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import { AppProps } from 'next/app';
+
+
+import '@/styles/style.scss';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+     <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
